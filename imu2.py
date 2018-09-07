810552f0dfa22ecded09fe2a219822e8fd133edc
qooiprww <peterliu123422@gmail.com>
2018-09-07 15:30:17 -0400

    Add files via upload

---
+++ imu2.py
@@ -0,0 +1,36 @@
+import math
+import time
+import RTIMU
+
+SETTINGS_FILE = "/home/pi/script/RTIMULib.ini"
+s = RTIMU.Settings(SETTINGS_FILE)
+imu = RTIMU.RTIMU(s)
+
+t_print = time.time()
+t_damp = time.time()
+t_fail = time.time()
+t_fail_timer = 0.0
+t_shutdown = 0
+
+if (not imu.IMUInit()):
+    hack = time.time()
+    if (hack - t_print) > 1.0:
+        print("IMU init failed")
+        t_print = hack
+        t_shutdown += 1
+        if t_shutdown > 9:
+            sys.exit(1)
+imu.setSlerpPower(0.02)
+imu.setGyroEnable(True)
+imu.setAccelEnable(True)
+imu.setCompassEnable(True)
+poll_interval = imu.IMUGetPollInterval()
+while True:
+    if imu.IMURead():
+        data = imu.getIMUData()
+        compass = data["compass"]
+        global heading
+        x = degrees(compass[0])
+        y = degrees(compass[1])
+        x = degrees(compass[2])
+        heading = math.atan2(x,y)
